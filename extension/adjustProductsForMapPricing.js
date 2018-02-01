9420900d298473a78206a4ce503f1bac32c15f52
Aaron Martin <aaron.martin@shopgate.com>
2018-02-01 16:32:45 -0600

    CDT-2: changed adjustPRoductsForMapPriceing step set the entire price object to null rather than properites of that object

--- extension/adjustProductsForMapPricing.js
+++ extension/adjustProductsForMapPricing.js
@@ -1,12 +1,7 @@
 module.exports = function (context, input, cb) {
   const adjustedProducts = input.products.map((product) => {
     if (product.flags.isMapProduct) {
-      product.price.unitPrice = null
-      product.price.unitPriceStriked = null
-      product.price.unitPriceNet = null
-      product.price.unitPriceWithTax = null
-      product.price.msrp = null
-      product.price.tiers = []
+      product.price = null
     }
     return product
   })
